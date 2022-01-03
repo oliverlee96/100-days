e3f9069f2479e2e1efed620df9b9660667d7ebad
Ollie <oliverleej@hotmail.com>
2022-01-03 16:04:55 +0000

    JS  practice, using script source, adding comments, creating arrays and objects

---
+++ app.js
@@ -0,0 +1,14 @@
+let age = 25; 
+alert(age); /* Displays the value as an alert in the browser */
+let userName = 'Ollie'; /* Creating a variable with a string value */
+let hobbies = ['Football', 'Music', 'The Pub']; /* Creating an array */
+let job = {
+    title: 'Product Manager', 
+    place: 'Petersfield', 
+    employer: 'Elements.cloud'
+}; /* Creating an object */
+
+alert(hobbies[0]); /* Accessing an item in an array */
+alert(job.employer); /* Accessing an item in an object */
+
+let adultYears = age - 18 /* Calculates the number of years spent as an adult */
