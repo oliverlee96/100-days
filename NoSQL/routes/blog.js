bee78050297060dee6a01b5ec8137fa3b21e7ea2
oliverlee96 <oliverleej@hotmail.com>
2023-02-17 11:03:37 +0000

    added routes for editing and deleting posts

--- NoSQL/routes/blog.js
+++ NoSQL/routes/blog.js
@@ -57,7 +57,57 @@ router.get('/posts/:id', async function (req,res) {
     return res.status(404).render('404');
   }
 
+  post.humanReadableDate = post.date.toLocaleDateString('en-UK', {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  });
+
+  post.date = post.date.toISOString();
+
   res.render('post-detail', {post: post})
+});
+
+router.get('/posts/:id/edit', async function(req,res) {
+  const postId = new ObjectId(req.params.id);
+
+  const post = await db
+  .getDb()
+  .collection('posts')
+  .findOne({_id: new ObjectId(postId)}, {title: 1, summary: 1, body:1});
+
+  if (!post) {
+    return res.status(404).render('404');
+  };
+
+  res.render('update-post', { post: post});
+});
+
+router.post("/posts/:id/edit", async function (req, res) {
+  const postId = new ObjectId(req.params.id);
+  const post = await db.getDb()
+  .collection('posts')
+  .updateOne({_id: postId}, {
+      $set: {
+        title: req.body.title, 
+        summary: req.body.summary, 
+        body: req.body.content,
+        date: new Date()
+      },
+    }
+  );
+
+  res.redirect('/posts');
+});
+
+router.post("/posts/:id/delete", async function (req, res) {
+  const postId = new ObjectId(req.params.id);
+  const post = await db.getDb()
+  .collection('posts')
+  .deleteOne({_id: postId});
+
+  res.redirect('/posts');
 });
 
 module.exports = router;
