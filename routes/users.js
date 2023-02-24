2b6c70e8f8721d98d31840bd032f1f2c93717398
oliverlee96 <oliverleej@hotmail.com>
2023-02-24 15:09:54 +0000

    file upload router using multer and saving in file

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,16 @@
 const express = require('express');
+const multer = require('multer');
 
+const storageConfig = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, 'images');
+  },
+  filename: function(req, file, cb) {
+    cb(null, Date.now() + '_' + file.originalname);
+  }
+});
+
+const upload = multer({ storage: storageConfig});
 const router = express.Router();
 
 router.get('/', function(req, res) {
@@ -8,6 +19,15 @@ router.get('/', function(req, res) {
 
 router.get('/new-user', function(req, res) {
   res.render('new-user');
+});
+
+router.post('/profiles', upload.single('image'), function (req, res) {
+  const uploadedImageFile = req.file;
+  const userData = req.body;
+
+  console.log(uploadedImageFile);
+  console.log(userData);
+  res.redirect('/');
 });
 
 module.exports = router;
